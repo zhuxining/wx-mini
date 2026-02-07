01fb126894c37b15b27acd359f074a1cb0b05fdf
syneju <syneju@gmail.com>
2026-02-07 13:52:14 +0800

    udpate dice ui

--- apps/web/src/hooks/use-lazy-ref.ts
+++ apps/web/src/hooks/use-lazy-ref.ts
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-function useLazyRef<T>(fn: () => T): React.RefObject<T> {
+function useLazyRef<T>(fn: () => T) {
 	const ref = React.useRef<T | null>(null);
+
 	if (ref.current === null) {
 		ref.current = fn();
 	}
+
 	return ref as React.RefObject<T>;
 }
 
